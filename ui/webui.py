ae77cfb30d24a4b26581a43b4957f8e99e941037
hmathpal <hmathpal@klick.com>
2023-03-26 00:22:11 -0400

    Webui add

---
+++ ui/webui.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from gpt_index import SimpleDirectoryReader,GPTListIndex,GPTSimpleVectorIndex,LLMPredictor,PromptHelper
+
+import time
+import os
+
+import sys
+sys.path.insert(0, '.')
+import config
+
+
+os.environ["OPENAI_API_KEY"] = config.OPEN_API_KEY
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    user_input = request.args.get('msg')+ " (give html formatted reply)"
+    vIndex = GPTSimpleVectorIndex.load_from_disk('/Users/hmathpal/Desktop/aiml/genai/vectorIndex.json')
+    response = vIndex.query(user_input, response_mode="compact")
+    time.sleep(1)
+    return str(response)
+
+if __name__ == "__main__":
+    app.run()
